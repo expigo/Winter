97682d5ecb35263cad2faddefe9ca52c27a30bb5
Kryspin Musiol <kryspin.musiol@gmail.com>
2019-02-20 00:21:32 +0100

    add @ComponentScan annotation

---
+++ src/main/java/com/kryspinmusiol/ioc/annotation/configuration/ComponentScan.java
@@ -0,0 +1,13 @@
+package com.kryspinmusiol.ioc.annotation.configuration;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ComponentScan {
+    String baseDir();
+
+}
