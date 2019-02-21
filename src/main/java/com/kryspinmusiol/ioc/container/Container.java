62fc019df66fbebb6a501aeaaa5f321c752c97c5
Kryspin Musiol <kryspin.musiol@gmail.com>
2019-02-21 23:46:05 +0100

    Add resolve method to Container class

--- src/main/java/com/kryspinmusiol/ioc/container/Container.java
+++ src/main/java/com/kryspinmusiol/ioc/container/Container.java
@@ -1,5 +1,6 @@
 package com.kryspinmusiol.ioc.container;
 
+import com.kryspinmusiol.ioc.annotation.Inject;
 import com.kryspinmusiol.ioc.annotation.configuration.ComponentScan;
 import com.kryspinmusiol.ioc.exception.ContainerException;
 import com.kryspinmusiol.ioc.loader.AnnotationBasedLoader;
@@ -7,6 +8,8 @@ import com.kryspinmusiol.ioc.loader.JsonClassPathLoader;
 import com.kryspinmusiol.ioc.loader.Loader;
 import org.reflections.Reflections;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Optional;
@@ -16,7 +19,7 @@ public class Container {
 
 
     private final Loader configurationLoader;
-    private Map<Class<?>, Supplier<?>> objectGraph = new HashMap<>();
+    private Map<Class<?>, Supplier<?>> objectGraph;
 
 
     private Container(Loader loader) {
@@ -25,13 +28,38 @@ public class Container {
     }
 
 
-    public static Container create(){
+    public static Container create() {
         return new Container(getAnnotationLoader());
     }
 
-    public static Container create(String configurationPath){
+    public static Container create(String configurationPath) {
         return new Container(new JsonClassPathLoader(configurationPath));
     }
+
+    public <T> T resolve(Class<T> cls) {
+
+        try {
+            final T t = cls.getConstructor().newInstance();
+            final Field[] declaredFields = cls.getDeclaredFields();
+
+            for (Field declaredField : declaredFields) {
+                if (declaredField.isAnnotationPresent(Inject.class)) {
+                    final Class<?> injectedFieldType = declaredField.getType();
+                    final Supplier<?> supplier = objectGraph.get(injectedFieldType);
+
+                    final Object objectToInject = supplier.get();
+                    declaredField.setAccessible(true);
+                    declaredField.set(t, objectToInject);
+                }
+            }
+
+            return t;
+        } catch (Exception e) {
+            throw new ContainerException("Cannot resolve dependency for class: " + cls, e.getCause());
+        }
+
+    }
+
 
     private static Loader getAnnotationLoader() {
         final String callerClassName = getCallerRootPackage();
@@ -57,11 +85,11 @@ public class Container {
 
     private static String getCallerRootPackage() {
 
-        
+
         StackTraceElement[] stElements = Thread.currentThread().getStackTrace();
-        for (int i=1; i<stElements.length; i++) {
+        for (int i = 1; i < stElements.length; i++) {
             StackTraceElement ste = stElements[i];
-            if (!ste.getClassName().equals(Container.class.getName()) && ste.getClassName().indexOf("java.lang.Thread")!=0) {
+            if (!ste.getClassName().equals(Container.class.getName()) && ste.getClassName().indexOf("java.lang.Thread") != 0) {
                 return ste.getClassName();
             }
         }
